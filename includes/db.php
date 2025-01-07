cb0975d155895402dfa8c71a94bc06f2990266fc
Dheevessh <dheevesshrao05@gmail.com>
2025-01-07 22:30:12 +0800

    a

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,11 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$password = '';
+$password = ''; // Replace with your MySQL password if set
 $database = 'cinema_booking';
 
 $conn = new mysqli($host, $user, $password, $database);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
