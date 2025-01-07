03f676e5cf1db5800c6acb0b22510ecf57341f8b
Dheevessh <dheevesshrao05@gmail.com>
2025-01-07 21:27:58 +0800

    a

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Script (js/script.js)
-// Example: Seat Selection Interactivity
-const seats = document.querySelectorAll('.seat');
-seats.forEach(seat => {
-    seat.addEventListener('click', () => {
-        seat.classList.toggle('selected');
-    });
-});
