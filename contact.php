7822191c7673f4712e22dca2a34f9aa6b4f24d6f
Dheevessh <dheevesshrao05@gmail.com>
2025-01-13 02:22:09 +0800

    a

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
-    <link rel="stylesheet" href="contact.css">
+    <link rel="stylesheet" href="contact1.css">
 </head>
 
 <body>
